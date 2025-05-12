236fb62c1c00b97483169162a3a925dc94baff26
Pranitha054 <154070623+Pranitha054@users.noreply.github.com>
2025-05-12 16:05:32 +0530

    Time: 39 ms (78.50%) | Memory: 12.6 MB (69.07%) - LeetSync

---
+++ 463-island-perimeter/island-perimeter.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def islandPerimeter(self, grid):
+        perimeter = 0
+        rows, cols = len(grid), len(grid[0])
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    perimeter += 4
+                    if i > 0 and grid[i-1][j] == 1:  # Check top
+                        perimeter -= 2
+                    if j > 0 and grid[i][j-1] == 1:  # Check left
+                        perimeter -= 2
+        return perimeter
