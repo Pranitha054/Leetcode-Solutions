5ce2ffe2fb47de3f28dad8c4bc46c6d9cd47d3db
Pranitha054 <154070623+Pranitha054@users.noreply.github.com>
2026-07-19 17:50:40 +0530

    Time: 1028 ms (5.04%) | Memory: 14.5 MB (8.08%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,6 +1,14 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        if sorted(s)==sorted(t):
+        if len(s)!=len(t):
+            return False
+        t=list(t)
+        for i in range(len(s)):
+            if s[i] not in t:
+                return False
+            else:
+                t.remove(s[i])
+        else:
             return True
-        return False
+
         
